6ec93de290e70ffa96d54082db66f3eb0b5d7e6c
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-26 21:38:26 +0500

    update index js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App'
+import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { createBook } from './actions';
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+store.dispatch(
+  createBook({ title: 'Think and Grow Rich', category: categories[5] })
+);
+store.dispatch(createBook({ title: 'O`tkan Kunlar', category: categories[1] }));
+store.dispatch(createBook({ title: 'Little Prince', category: categories[4] }));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
