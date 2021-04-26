5c1255e8c70d5b53a58e1ff398558109d07a08d2
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-26 21:35:48 +0500

    update Book component

---
+++ src/components/Book.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({ id, title, category, removeBook }) => {
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{category}</td>
+      <td>
+        <button onClick={() => removeBook(id)}>Remove</button>
+      </td>
+    </tr>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  category: PropTypes.string,
+  removeBook: PropTypes.func,
+};
+
+Book.defaultProps = {
+  id: null,
+  title: '',
+  category: '',
+};
+
+export default Book;
