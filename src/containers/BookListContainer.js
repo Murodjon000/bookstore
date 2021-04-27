806b2ff39a5c3dce4b493f8b996af8f410ecad9a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-27 21:25:20 +0500

    update BooksListContainer with Filter actions

--- src/containers/BookListContainer.js
+++ src/containers/BookListContainer.js
@@ -1,13 +1,23 @@
 import { connect } from 'react-redux';
 import { removeBook } from '../actions/index';
 import BookList from '../components/BookList';
+import { filterBook } from '../actions/index';
+
+const getBooks = (books, filter) => {
+  if (filter === 'All') {
+    return books;
+  }
+  return books.filter((book) => book.category === filter);
+};
 
 const mapStateToProps = (state) => ({
-  books: state.books,
+  books: getBooks(state.books, state.filter),
+  filter: state.filter,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   handleRemoveBook: (id) => dispatch(removeBook(id)),
+  filterBook: (category) => dispatch(filterBook(category)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
