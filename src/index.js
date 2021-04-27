d387e9bed4af1ac124d77d7ba307ce335db252f3
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-27 21:25:50 +0500

    update index js file

--- src/index.js
+++ src/index.js
@@ -5,19 +5,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import rootReducer from './reducers';
-import { createBook } from './actions';
+import { createBook, filterBook } from './actions';
+import categories from './helper/category';
 
 const store = createStore(rootReducer);
 
-const categories = [
-  'Action',
-  'Biography',
-  'History',
-  'Horror',
-  'Kids',
-  'Learning',
-  'Sci-Fi',
-];
 store.dispatch(createBook({ title: 'React', category: categories[5] }));
 store.dispatch(createBook({ title: 'O`tkan Kunlar', category: categories[1] }));
 store.dispatch(createBook({ title: 'Little Prince', category: categories[4] }));
