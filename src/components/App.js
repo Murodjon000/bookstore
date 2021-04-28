19166badf116beb26e24abfbde7ab2f5d9fdbafc
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-28 17:25:55 +0500

    update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BookListContainer from '../containers/BookListContainer';
 import BooksFormContainer from '../containers/BooksFormContainer';
+import Header from './Header';
 
 const App = () => (
   <div>
