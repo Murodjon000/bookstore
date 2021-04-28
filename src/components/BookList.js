23731e757d38d6fc5167a7fab3fa727ecf13ad83
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-28 17:26:25 +0500

    update BookList component with UI styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,28 +8,18 @@ const BookList = (
 ) => (
   <div>
     <CategoryFilter filter={filter} handleFilterChange={filterBook} />
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove Book</th>
-        </tr>
-      </thead>
 
-      <tbody>
-        {books.map((book) => (
-          <BookContainer
-            id={book.id}
-            title={book.title}
-            category={book.category}
-            key={book.id}
-            handleRemoveBook={handleRemoveBook}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="books__container">
+      {books.map((book) => (
+        <BookContainer
+          id={book.id}
+          title={book.title}
+          category={book.category}
+          key={book.id}
+          handleRemoveBook={handleRemoveBook}
+        />
+      ))}
+    </div>
   </div>
 );
 
